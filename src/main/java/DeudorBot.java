fa919a75d291e8d3b9d0c2f838b5f1d0c58fd276
pbalbi <pablo.balbi@mercadolibre.com>
2018-08-25 18:15:51 -0300

    Adding loggers

--- src/main/java/DeudorBot.java
+++ src/main/java/DeudorBot.java
@@ -1,12 +1,20 @@
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
+
 public class DeudorBot extends TelegramLongPollingBot {
+
+	private static final Logger log = LogManager.getLogger(DeudorBot.class);
 
 	public void onUpdateReceived(Update update) {
 		if (update.hasMessage() && update.getMessage().hasText()) {
+
+			log.info("Message arrived from chatId: " + update.getMessage().getChatId().toString());
+
 			SendMessage message = new SendMessage()
 				.setChatId(update.getMessage().getChatId())
 				.setText(update.getMessage().getText());
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,17 +1,24 @@
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.telegram.telegrambots.ApiContextInitializer;
 import org.telegram.telegrambots.meta.TelegramBotsApi;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
 public class Main {
 
+	private static final Logger log = LogManager.getLogger(Main.class);
+
 	public static void main(String[] args) {
+
 		ApiContextInitializer.init();
 
 		TelegramBotsApi botsApi = new TelegramBotsApi();
 
 		try {
+			log.info("Staring DeudorBot...");
 			botsApi.registerBot(new DeudorBot());
 		} catch (TelegramApiException e) {
+			log.fatal("Error in Deudor execution or starting: " + e.toString());
 			e.printStackTrace();
 		}
 
