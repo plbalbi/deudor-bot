5e29a3e1444ff98853bd7ba9c662bc0650a665ef
pbalbi <pablo.balbi@mercadolibre.com>
2018-09-02 13:48:56 -0300

    Reordering method

--- src/main/java/domain/SpendsSession.java
+++ src/main/java/domain/SpendsSession.java
@@ -6,25 +6,24 @@ public class SpendsSession {
 
 	private Map<String, Float> balance = new HashMap<String, Float>();
 
-	public SpendsSession() {
-	}
-
-	public boolean isClean() {
-		return balance.isEmpty();
-	}
+	public SpendsSession() {}
 
 	public void spend(String aName, float anAmount) {
 		Float actualSpends = balance.getOrDefault(aName, 0.0f);
 		balance.put(aName, actualSpends + anAmount);
 	}
 
-	public float getBalanceOf(String aName){
-		return balance.getOrDefault(aName, 0.0f);
-	}
-
 	public void pay(String aName, float anAmount) {
 		Float actualSpends = balance.getOrDefault(aName, 0.0f);
 		balance.put(aName, actualSpends - anAmount);
+	}
+
+	public boolean isClean() {
+		return balance.isEmpty();
+	}
+
+	public float getBalanceOf(String aName){
+		return balance.getOrDefault(aName, 0.0f);
 	}
 
 	public List<Debt> end() {
